bf8adda18addf34ea2503bfc56ddebd0b8d0b4bb
SKProCH <shadowkillerpropro@yandex.ru>
2020-09-06 20:27:19 +0300

    Add base theme switcher to demo project

--- AvaloniaRipple/MainWindow.xaml.cs
+++ AvaloniaRipple/MainWindow.xaml.cs
@@ -1,20 +1,33 @@
-﻿using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using MaterialXamlToolKit.Avalonia.Themes;
+using MaterialXamlToolKit.Avalonia.Themes.Base;
 
-namespace AvaloniaRipple
-{
-    public class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace AvaloniaRipple {
+    public class MainWindow : Window {
+        private PaletteHelper _paletteHelper;
+
+        public MainWindow() {
             InitializeComponent();
-
         }
 
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
+            _paletteHelper = new PaletteHelper();
+            this.FindControl<CheckBox>("BaseThemeCheckBox").IsChecked = _paletteHelper.GetTheme().GetBaseTheme() == BaseThemeMode.Dark;
+        }
+
+        void BaseThemeChanged(object sender, RoutedEventArgs args) {
+            if (sender is CheckBox checkBox) {
+                var theme = _paletteHelper.GetTheme();
+                var baseThemeMode = checkBox.IsChecked!.Value switch {
+                    true  => BaseThemeMode.Dark,
+                    false => BaseThemeMode.Light
+                };
+                theme.SetBaseTheme(baseThemeMode.GetBaseTheme());
+                _paletteHelper.SetTheme(theme);
+            }
         }
     }
 }
