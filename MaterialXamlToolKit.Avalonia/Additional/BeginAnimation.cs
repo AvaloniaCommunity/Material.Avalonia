9f04c492379bbbbad0254044480335a75f5ac24e
SKProCH <shadowkillerpropro@yandex.ru>
2020-09-15 10:25:35 +0300

    Fix RadioButton style. Add ControllableAnimationBase and BeginAnimation

---
+++ MaterialXamlToolKit.Avalonia/Additional/BeginAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reactive.Subjects;
+
+namespace MaterialXamlToolKit.Avalonia.Additional {
+    /// <summary>
+    /// Tracks the progress of an animation. Will always continue to end or until restart.
+    /// Perhaps this has already been implemented without such crutches. Issue: https://github.com/AvaloniaUI/Avalonia/issues/4673
+    /// </summary>
+    public class BeginAnimation : ControllableAnimationBase {
+        internal override void OnNext(Subject<bool> match, bool previous, bool obj) {
+            if (obj) return;
+            // "Turning" off
+            match.OnNext(false);
+            
+            // Then "turning" on
+            match.OnNext(true);
+        }
+    }
+}
---
+++ MaterialXamlToolKit.Avalonia/Additional/ControllableAnimationBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Metadata;
+
+namespace MaterialXamlToolKit.Avalonia.Additional {
+    /// <summary>
+    /// Tracks the progress of an animation. This class should act the same as <see cref="Animation"/>
+    /// </summary>
+    public class ControllableAnimationBase : AvaloniaObject, IAnimation {
+        /// <summary>
+        /// Defines the <see cref="Duration"/> property.
+        /// </summary>
+        public static readonly DirectProperty<ControllableAnimationBase, Animation> AnimationProperty =
+            AvaloniaProperty.RegisterDirect<ControllableAnimationBase, Animation>(
+                nameof(_animation),
+                o => o._animation,
+                (o, v) => o._animation = v);
+
+        private Animation _animation;
+
+        [Content]
+        public Animation Animation {
+            get => _animation;
+            set => SetAndRaise(AnimationProperty, ref _animation, value);
+        }
+
+        public virtual IDisposable Apply(Animatable control, IClock clock, IObservable<bool> match, Action onComplete = null) {
+            var previous = false;
+            var observable = new Subject<bool>();
+            match.Subscribe(b => {
+                OnNext(observable, previous, b);
+                previous = b;
+            });
+            return Animation.Apply(control, clock, observable, onComplete);
+        }
+
+        internal virtual void OnNext(Subject<bool> match, bool previous, bool obj) {
+            match.OnNext(obj);
+        }
+
+        public virtual Task RunAsync(Animatable control, IClock clock) {
+            return Animation.RunAsync(control, clock);
+        }
+    }
+}
