667b6a72f17146e340a4682557f6ce45a79936e7
SKProCH <shadowkillerpropro@yandex.ru>
2020-09-16 21:18:49 +0300

    Fix ListBox and ListBoxItem style

---
+++ MaterialXamlToolKit.Avalonia/Converters/BrushRoundConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace MaterialXamlToolKit.Avalonia.Converters {
+    public class BrushRoundConverter : IValueConverter {
+        public static readonly IValueConverter Instance = new BrushRoundConverter();
+        public Brush HighValue { get; set; } = new SolidColorBrush(Brushes.White.Color);
+
+        public Brush LowValue { get; set; } = new SolidColorBrush(Brushes.Black.Color);
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is SolidColorBrush solidColorBrush)) return null;
+
+            var color = solidColorBrush.Color;
+
+            var brightness = 0.3 * color.R + 0.59 * color.G + 0.11 * color.B;
+
+            return brightness < 123 ? LowValue : HighValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
