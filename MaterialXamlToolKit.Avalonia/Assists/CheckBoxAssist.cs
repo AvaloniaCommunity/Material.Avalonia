353f061f949e04e8fa83d05109c51db16d8e35b2
SKProCH <skproch@yandex.ru>
2020-09-20 11:29:47 +0300

    Rework CheckBox style

---
+++ MaterialXamlToolKit.Avalonia/Assists/CheckBoxAssist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+
+namespace MaterialXamlToolKit.Avalonia.Assists {
+    public static class CheckBoxAssist {
+        private const double DefaultCheckBoxSize = 18.0;
+
+        #region AttachedProperty : CheckBoxSizeProperty
+
+        public static readonly AvaloniaProperty<double> CheckBoxSizeProperty
+            = AvaloniaProperty.RegisterAttached<CheckBox, double>("CheckBoxSize", typeof(CheckBoxAssist), DefaultCheckBoxSize);
+
+        public static double GetCheckBoxSize(CheckBox element) => (double) element.GetValue(CheckBoxSizeProperty);
+        public static void SetCheckBoxSize(CheckBox element, double checkBoxSize) => element.SetValue(CheckBoxSizeProperty, checkBoxSize);
+
+        #endregion
+    }
+}
