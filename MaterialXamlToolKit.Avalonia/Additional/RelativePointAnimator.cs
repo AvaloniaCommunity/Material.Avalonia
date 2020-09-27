c1acce48856e785bf14660356365388756dba854
SKProCH <skproch@yandex.ru>
2020-09-27 08:53:33 +0300

    Fix ProgressBar style. Add RelativePointAnimator and TransitionAssist

---
+++ MaterialXamlToolKit.Avalonia/Additional/RelativePointAnimator.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Animation.Animators;
+
+namespace MaterialXamlToolKit.Avalonia.Additional {
+    public class RelativePointAnimator : Animator<RelativePoint> {
+        public override RelativePoint Interpolate(double progress, RelativePoint oldValue, RelativePoint newValue) {
+            return new RelativePoint(
+                (newValue.Point.X - oldValue.Point.X) * progress + oldValue.Point.X,
+                (newValue.Point.Y - oldValue.Point.Y) * progress + oldValue.Point.Y,
+                newValue.Unit);
+        }
+    }
+}
--- MaterialXamlToolKit.Avalonia/MaterialToolKit.xaml.cs
+++ MaterialXamlToolKit.Avalonia/MaterialToolKit.xaml.cs
@@ -1,8 +1,16 @@
+using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Markup.Xaml;
 using Avalonia.Styling;
+using MaterialXamlToolKit.Avalonia.Additional;
 
 namespace MaterialXamlToolKit.Avalonia
 {
     public class MaterialToolKit : Styles
     {
+        public MaterialToolKit() {
+            AvaloniaXamlLoader.Load(this);
+            Animation.RegisterAnimator<RelativePointAnimator>(property => typeof(RelativePoint).IsAssignableFrom(property.PropertyType));
+        }
     }
 }
