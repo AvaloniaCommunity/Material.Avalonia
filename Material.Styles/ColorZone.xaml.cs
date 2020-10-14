8b484d27b73b609756ecff142dc7839d24ca5a41
SKProCH <skproch@yandex.ru>
2020-10-14 21:41:52 +0300

    [WIP] Add ColorZone

---
+++ Material.Styles/ColorZone.xaml.cs
@@ -0,0 +1,37 @@
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Material.Styles {
+    public enum ColorZoneMode
+    {
+        Standard,
+        Inverted,
+        PrimaryLight,
+        PrimaryMid,
+        PrimaryDark,
+        Accent,
+        Light,
+        Dark,
+        Custom
+    }
+    public class ColorZone : ContentControl {
+        public static readonly StyledProperty<CornerRadius> CornerRadiusProperty =
+            AvaloniaProperty.Register<ColorZone, CornerRadius>(nameof(CornerRadius));
+
+        /// <summary>
+        /// Gets or sets the radius of the border rounded corners.
+        /// </summary>
+        public CornerRadius CornerRadius {
+            get { return GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+        
+        public static readonly StyledProperty<ColorZoneMode> ModeProperty = AvaloniaProperty.Register<ColorZone, ColorZoneMode>(nameof(Mode));
+
+        public ColorZoneMode Mode
+        {
+            get { return (ColorZoneMode)GetValue(ModeProperty); }
+            set { SetValue(ModeProperty, value); }
+        }
+    }
+}
