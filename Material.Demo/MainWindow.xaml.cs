426ca546e71117662e1afd71900c132e8e568c23
lary <f98f96@gmail.com>
2020-10-17 16:38:07 +0300

    Restyle demo app

--- Material.Demo/MainWindow.xaml.cs
+++ Material.Demo/MainWindow.xaml.cs
@@ -3,8 +3,11 @@ using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Material.Colors;
 using Material.Styles.Themes;
 using Material.Styles.Themes.Base;
+using PrimaryColor = Material.Colors.PrimaryColor;
 
 namespace Material.Demo {
     public class MainWindow : Window {
@@ -18,17 +21,28 @@ namespace Material.Demo {
         private void InitializeComponent() {
             AvaloniaXamlLoader.Load(this);
             _paletteHelper = new PaletteHelper();
-            this.FindControl<CheckBox>("BaseThemeCheckBox").IsChecked = _paletteHelper.GetTheme().GetBaseTheme() == BaseThemeMode.Dark;
+            this.FindControl<ToggleSwitch>("BaseThemeCheckBox").IsChecked = _paletteHelper.GetTheme().GetBaseTheme() == BaseThemeMode.Dark;
         }
 
         private void BaseThemeChanged(object sender, RoutedEventArgs args) {
-            if (sender is CheckBox checkBox) {
+            if (sender is ToggleSwitch checkBox) {
                 var theme = _paletteHelper.GetTheme();
                 var baseThemeMode = checkBox.IsChecked!.Value switch {
                     true  => BaseThemeMode.Dark,
                     false => BaseThemeMode.Light
                 };
                 theme.SetBaseTheme(baseThemeMode.GetBaseTheme());
+                _paletteHelper.SetTheme(theme);
+            }
+        }
+        private void BaseThemeColorChanged(object sender, RoutedEventArgs args) {
+            if (sender is ToggleSwitch checkBox) {
+                var theme = _paletteHelper.GetTheme();
+                var color = checkBox.IsChecked!.Value switch {
+                    true  => PrimaryColor.Purple,
+                    false => PrimaryColor.Teal
+                };
+                theme.SetPrimaryColor(SwatchHelper.Lookup[(MaterialColor) color]);
                 _paletteHelper.SetTheme(theme);
             }
         }
