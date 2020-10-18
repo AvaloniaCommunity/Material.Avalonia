fd43682fdd41e66c94bb2e06d58e91844f4c4ece
SKProCH <skproch@yandex.ru>
2020-10-18 09:54:26 +0300

    Rename CheckBoxAssist to SelectionControlAssist, change CheckBox default size

---
+++ Material.Styles/Assists/SelectionControlAssist.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace Material.Styles.Assists {
+    public static class SelectionControlAssist {
+        public static readonly AvaloniaProperty<double> SizeProperty
+            = AvaloniaProperty.RegisterAttached<Button, double>("Size", typeof(SelectionControlAssist));
+
+        public static double GetSize(Button element) {
+            return (double) element.GetValue(SizeProperty);
+        }
+
+        public static void SetSize(Button element, double checkBoxSize) {
+            element.SetValue(SizeProperty, checkBoxSize);
+        }
+    }
+}
