b5814e340c2f4624222371ba363c653df7db493a
AppleNeko2001 <appleneko2001@outlook.com>
2020-12-03 15:40:01 +0900

    Add light, dark, accent, mini and extend styles of FAB widget, transition still incomplete.

--- Material.Styles/FloatingButton.xaml.cs
+++ Material.Styles/FloatingButton.xaml.cs
@@ -1,10 +1,23 @@
+using Avalonia.Animation;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+using Avalonia.Styling;
+using System.Threading.Tasks;
 
 namespace Material.Styles
 {
-    public class FloatingButton:Button
-    
-    {
-        
+    public class FloatingButton : Button
+    { 
+        public FloatingButton() {
+            this.AttachedToVisualTree += FloatingButton_Initialized; 
+        } 
+        private async void FloatingButton_Initialized(object sender, System.EventArgs e)
+        { 
+            ScaleTransform t = (ScaleTransform)(this.RenderTransform = new ScaleTransform(0, 0));
+
+            t?.SetValue(ScaleTransform.ScaleXProperty, 1);
+            t?.SetValue(ScaleTransform.ScaleYProperty, 1);
+        }
     }
 }
