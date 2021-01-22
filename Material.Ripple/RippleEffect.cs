6993a8e88b8a746ef12627cfd60d88ed4d0cbf75
AppleNeko2001 <appleneko2001@outlook.com>
2021-01-23 03:43:52 +0900

    Fix .notransitions feature

--- Material.Ripple/RippleEffect.cs
+++ Material.Ripple/RippleEffect.cs
@@ -4,6 +4,7 @@ using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Shapes;
+using Avalonia.Input;
 using Avalonia.Media;
 using Avalonia.Styling;
 
@@ -17,8 +18,17 @@ namespace Material.Ripple {
         private IAnimationSetter _toWidth;
 
         public RippleEffect() {
+            AddHandler(PointerReleasedEvent, async (s, e) => {
+                if (Classes.Contains("notransitions"))
+                    NoTransitionsReleased();
+            });
             AddHandler(PointerPressedEvent, async (s, e) => {
                 if (_isRunning) return;
+                if (Classes.Contains("notransitions"))
+                {
+                    NoTransitionsPressed(e);
+                    return;
+                }
                 var pointer = e.GetPosition(this);
                 _isRunning = true;
                 var maxWidth = Math.Max(Bounds.Width, Bounds.Height) * 2.2D;
@@ -30,6 +40,19 @@ namespace Material.Ripple {
                 _circle.Opacity = 0;
                 _isRunning = false;
             });
+        }
+        
+        private void NoTransitionsReleased()
+        {
+            _circle.Opacity = 0;
+            _circle.Width = 0;
+        }
+        private void NoTransitionsPressed(PointerPressedEventArgs e)
+        {
+            var pointer = e.GetPosition(this);
+            _circle.Opacity = 1;
+            _circle.Width = Math.Max(Bounds.Width, Bounds.Height) * 2.2D;
+            _circle.Margin = new Thickness(pointer.X - _circle.Width / 2, pointer.Y - _circle.Width / 2, 0, 0);
         }
 
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {
