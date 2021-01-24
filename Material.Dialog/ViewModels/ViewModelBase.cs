e8fad4ac4e62ccd7aa8ee6d7389bdc447a1783a4
AppleNeko2001 <appleneko2001@outlook.com>
2021-01-24 12:50:50 +0900

    Add namespace to avoid conflict ViewModelBase classes.

--- Material.Dialog/ViewModels/ViewModelBase.cs
+++ Material.Dialog/ViewModels/ViewModelBase.cs
@@ -1,9 +1,13 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices; 
-public class ViewModelBase : INotifyPropertyChanged
-{
-    public event PropertyChangedEventHandler PropertyChanged;
 
-    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+namespace Material.Dialog
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
