3953bece08bdadda017a8ae187a0591be08a3a0a
AppleNeko2001 <appleneko2001@outlook.com>
2021-01-28 22:32:53 +0900

    Add PackIcon markup support

---
+++ Material.Styles/Extensions/PackIconExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Avalonia.Markup.Xaml;
+
+namespace Material.Styles.Extensions
+{
+    public class PackIconExtension : MarkupExtension
+    {
+        public PackIconExtension()
+        { }
+
+        public PackIconExtension(PackIconKind kind)
+        {
+            Kind = kind;
+        }
+
+        public PackIconExtension(PackIconKind kind, double size)
+        {
+            Kind = kind;
+            Size = size;
+        }
+
+        [ConstructorArgument("kind")]
+        public PackIconKind Kind { get; set; }
+
+        [ConstructorArgument("size")]
+        public double? Size { get; set; } = 24;
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var result = new PackIcon { Kind = Kind };
+
+            if (Size.HasValue)
+            {
+                result.Height = Size.Value;
+                result.Width = Size.Value;
+            }
+
+            return result;
+        }
+    }
+}
