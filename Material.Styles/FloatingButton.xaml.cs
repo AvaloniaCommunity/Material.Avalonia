c246b225e3a89cbc609bfafda6e7684217324b75
AppleNeko2001 <appleneko2001@outlook.com>
2021-02-07 05:40:52 +0900

    Add CreateFloatingButton function and update code

--- Material.Styles/FloatingButton.xaml.cs
+++ Material.Styles/FloatingButton.xaml.cs
@@ -5,6 +5,7 @@ using Avalonia.Controls.Primitives;
 using Avalonia.Media;
 using Avalonia.Styling;
 using System.Threading.Tasks;
+using Avalonia.Layout;
 
 namespace Material.Styles
 {
@@ -15,11 +16,47 @@ namespace Material.Styles
 
         public bool IsExtended
         {
-            get { return GetValue(IsExtendedProperty); }
-            set { SetValue(IsExtendedProperty, value); }
+            get => GetValue(IsExtendedProperty);
+            set => SetValue(IsExtendedProperty, value);
         }
 
         public FloatingButton() { 
         }  
+        
+        /// <summary>
+        /// Create FAB by easy way.
+        /// </summary>
+        /// <param name="iconKind"></param>
+        /// <param name="text"></param>
+        /// <returns>Instance of new FAB. <br/>
+        /// PackIcon element name is PART_Icon and
+        /// TextBlock name is PART_AdditionalText</returns>
+        public static FloatingButton CreateFloatingButton(PackIconKind iconKind, string text = null)
+        {
+            FloatingButton button = new FloatingButton();
+            button.Content = new StackPanel()
+            {
+                Orientation = Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Children =
+                {
+                    new PackIcon()
+                    {
+                        Name = "PART_Icon",
+                        Kind = (PackIconKind)iconKind,
+                        Width = 24, Height = 24
+                    },
+                    new TextBlock()
+                    {
+                        Name = "PART_AdditionalText",
+                        Classes = Classes.Parse("Button"),
+                        Margin = new Thickness(12,0,0,0),
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Text = text,
+                    }
+                }
+            };
+            return button;
+        }
     }
 }
