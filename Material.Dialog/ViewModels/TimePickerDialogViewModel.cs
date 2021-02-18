cc822079a095f08971348dd891b3241509b2a65b
AppleNeko2001 <appleneko2001@outlook.com>
2021-02-18 20:47:04 +0900

    Complete scratch of TimePicker, still incomplete

--- Material.Dialog/ViewModels/TimePickerDialogViewModel.cs
+++ Material.Dialog/ViewModels/TimePickerDialogViewModel.cs
@@ -6,6 +6,7 @@ using Material.Dialog.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Avalonia.Media;
 
 namespace Material.Dialog.ViewModels
 {
@@ -21,6 +22,106 @@ namespace Material.Dialog.ViewModels
 
         private DialogResultButton m_NegativeButton;
         public DialogResultButton NegativeButton { get => m_NegativeButton; internal set => m_NegativeButton = value; }
+
+        private ushort _firstField = 0;
+        public ushort FirstField
+        {
+            get => _firstField;
+            set
+            {
+                _firstField = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private ushort _secondField = 0;
+        public ushort SecondField
+        {
+            get => _secondField;
+            set
+            {
+                _secondField = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _firstPanelPointerAngle;
+        public double FirstPanelPointerAngle
+        {
+            get => _firstPanelPointerAngle;
+            set
+            {
+                _firstPanelPointerAngle = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        private double _secondPanelPointerAngle;
+        public double SecondPanelPointerAngle
+        {
+            get => _secondPanelPointerAngle;
+            set
+            {
+                _secondPanelPointerAngle = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isAm = true;
+        public bool IsAm
+        {
+            get => _isAm;
+            set
+            {
+                _isAm = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _carouselIndex = 0;
+        public int CarouselIndex
+        {
+            get => _carouselIndex;
+            set
+            {
+                _carouselIndex = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FirstFieldSelected));
+                OnPropertyChanged(nameof(SecondFieldSelected));
+            }
+        }
+
+        public bool FirstFieldSelected
+        {
+            get => _carouselIndex == 0;
+            set
+            { 
+                CarouselIndex = value ? 0 : 1; 
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool SecondFieldSelected
+        {
+            get => _carouselIndex == 1;
+            set
+            {
+                CarouselIndex = value ? 1 : 0;
+                OnPropertyChanged();
+            }
+        }
+
+        public void SetFirstField(int v)
+        {
+            FirstField = (ushort)v;
+            FirstPanelPointerAngle = (v / (double)12) * 360;
+        }
+        
+        public void SetSecondField(int v)
+        {
+            SecondField = (ushort)v;
+            SecondPanelPointerAngle = (v / (double)60) * 360;
+        }
 
         public TimePickerDialogViewModel(TimePickerDialog dialog)
         {
--- Material.Dialog/Views/TimePickerDialog.axaml.cs
+++ Material.Dialog/Views/TimePickerDialog.axaml.cs
@@ -1,13 +1,78 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using Material.Dialog.Interfaces;
+using Material.Dialog.ViewModels;
 
 namespace Material.Dialog.Views
 {
     public class TimePickerDialog : Window, IDialogWindowResult<DateTimePickerDialogResult>
     {
+        //private bool PointerHoldingCell;
+        private TimePickerDialogViewModel viewModel;
+        private Grid CallerPanel1;
+        private Grid CallerPanel2;
+        
+        private void CreateCallerCells1() => CreateCallerCellsToPanel(CallerPanel1, 12, firstText: 12, onPointerPressing: v => viewModel.SetFirstField(v));
+        private void CreateCallerCells2() => CreateCallerCellsToPanel(CallerPanel2, 60, onPointerPressing: v => viewModel.SetSecondField(v));
+        
+        private void CreateCallerCellsToPanel(Grid panel, int counts, float radius = 109, int firstText = 0, Action<int> onPointerPressing = null)
+        {
+            var offset = (Math.PI * 2) * 0.25; 
+            for (var i = 0; i < counts; i++)
+            {
+                var r = (float) i / (float) counts * (Math.PI * 2) - offset;
+                var x = radius * Math.Cos(r);
+                var y = radius * Math.Sin(r);
+
+                var v = i;
+                var text = i == 0 ? firstText.ToString() : v.ToString();
+                var cell = CreateCallerCell(x, y, text, v, i1 => onPointerPressing?.Invoke(i1));
+                panel.Children.Add(cell);
+            }
+        }
+
+        private Control CreateCallerCell(double x, double y, string text, int value, Action<int> onPointerPressing)
+        {
+            var root = new Border()
+            {
+                Classes = Classes.Parse("CallerCell"),
+                RenderTransform = new TranslateTransform(x, y),
+                Background = SolidColorBrush.Parse("Transparent"),
+                Tag = value,
+                Child = new Grid()
+                {
+                    Children = {
+                        new Border()
+                        {
+                            Name = "PointerEnterFeedback",
+                        },
+                        new TextBlock()
+                        {
+                            Text = text
+                        }
+                    }
+                }
+            }; 
+            
+            root.PointerPressed += (sender, args) =>
+            {
+                //PointerHoldingCell = true;
+                onPointerPressing((int) root.Tag);
+            };
+            root.PointerReleased += (sender, args) =>
+            {
+                var pager = this.Get<Carousel>("PART_PagesRoot");
+                pager.Next();
+            };
+            return root;
+        }
+        
+    
         public DateTimePickerDialogResult Result { get; set; }
         
         public TimePickerDialog()
@@ -15,8 +80,24 @@ namespace Material.Dialog.Views
             Result = new DateTimePickerDialogResult();
             
             InitializeComponent();
+            CallerPanel1 = this.Get<Grid>(nameof(CallerPanel1));
+            CallerPanel2 = this.Get<Grid>(nameof(CallerPanel2));
         }
-        
+
+        public void AttachViewModel(TimePickerDialogViewModel vm)
+        {
+            this.DataContext = vm;
+            viewModel = vm;
+        }
+
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+            
+            CreateCallerCells1();
+            CreateCallerCells2();
+        }
+
         public DateTimePickerDialogResult GetResult() => Result;
         
         public void SetNegativeResult(DialogResult result) => Result._result = result.GetResult;
