064cca5cc5f64d90d4b0be6ff69e29ca26e7bc6f
AppleNeko2001 <appleneko2001@outlook.com>
2021-03-02 08:32:08 +0900

    Add discrete automover (it's pretty buggy)

---
+++ Material.Styles/Converters/AutoCorrectPositionConverter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using Avalonia.Media.Transformation;
+using Avalonia.VisualTree;
+
+namespace Material.Styles.Converters
+{
+    public class AutoCorrectPositionConverter : IValueConverter
+    {
+        public static double DefaultOffsetY = 0;
+        
+        private static double GetOffLeft(Rect bounds, double offsetX) => offsetX - bounds.Width / 2;
+
+        private static double GetOffRight(Rect bounds, double windowW, double offsetX) => offsetX + (bounds.Width + bounds.Width / 2.0) - windowW;
+
+        private static Vector GetTranslate(TransformedBounds bounds)
+        {
+            return new Vector(bounds.Transform.M31, bounds.Transform.M32);
+        }
+
+        private Vector _prevCorrect = Vector.One;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var transformedBounds = value as TransformedBounds?;
+            
+            double offsetX = 0;
+            if (transformedBounds.HasValue)
+            {
+                var bounds = transformedBounds.Value;
+                
+                var translate = GetTranslate(bounds);
+
+                var left = GetOffLeft(bounds.Bounds, translate.X - _prevCorrect.X);
+                var right = GetOffRight(bounds.Bounds, bounds.Clip.Width, translate.X- _prevCorrect.X);
+
+                if (left < 0)
+                {
+                    offsetX = -left;
+                    //_prevCorrect = new Vector(offsetX, DefaultOffsetY);
+                }
+                else if (right > 0)
+                {
+                    offsetX = -right; 
+                   // _prevCorrect = new Vector(offsetX, DefaultOffsetY);
+                }
+            }
+            return new TranslateTransform(offsetX, DefaultOffsetY);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
