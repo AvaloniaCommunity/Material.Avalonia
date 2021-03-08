9ea962d7144e9c072519b208d963760acd48ac31
AppleNeko2001 <appleneko2001@outlook.com>
2021-03-08 13:43:41 +0900

    Improve time picker

---
+++ Material.Dialog/Converters/HourStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Material.Dialog.Converters
+{
+    public class HourStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = value as ushort?;
+            if (v.HasValue)
+            {
+                if (v.Value == 0)
+                {
+                    return 12.ToString();
+                }
+
+                return v.Value.ToString();
+            }
+
+            return 0.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
