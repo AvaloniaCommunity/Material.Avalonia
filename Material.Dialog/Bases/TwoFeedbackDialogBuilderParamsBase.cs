a299e5ed1ba3293e1e5c88dd11ff6da99062666e
AppleNeko2001 <appleneko2001@outlook.com>
2021-03-08 13:42:25 +0900

    Add new parameters base class

---
+++ Material.Dialog/Bases/TwoFeedbackDialogBuilderParamsBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Material.Dialog.Interfaces;
+
+namespace Material.Dialog.Bases
+{
+    public class TwoFeedbackDialogBuilderParamsBase : DialogWindowBuilderParamsBase
+    {
+        /// <summary>
+        /// Define positive action button.
+        /// </summary>
+        public DialogResultButton PositiveButton = DialogHelper.CreateSimpleDialogButtons(Enums.DialogButtonsEnum.Ok)[0];
+
+        /// <summary>
+        /// Define negative action button.
+        /// <br/>You still have a way go back lmao.
+        /// </summary>
+        public DialogResultButton NegativeButton = DialogHelper.CreateSimpleDialogButtons(Enums.DialogButtonsEnum.OkCancel)[0];
+    }
+}
