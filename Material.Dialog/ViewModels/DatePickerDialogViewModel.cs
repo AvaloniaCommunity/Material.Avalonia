da6acb004b5aeb50edc614473e0e0e78b1eedb57
AppleNeko2001 <appleneko2001@outlook.com>
2021-03-10 23:17:36 +0900

    Implement base members to DatePicker view model

--- Material.Dialog/ViewModels/DatePickerDialogViewModel.cs
+++ Material.Dialog/ViewModels/DatePickerDialogViewModel.cs
@@ -1,7 +1,39 @@
-﻿namespace Material.Dialog.ViewModels
+﻿using Avalonia.Threading;
+using Material.Dialog.Commands;
+using Material.Dialog.Views;
+
+namespace Material.Dialog.ViewModels
 {
     public class DatePickerDialogViewModel : DialogWindowViewModel
     {
+        private DatePickerDialog _window;
+
+        private DialogResultButton _positiveButton;
+        public DialogResultButton PositiveButton { get => _positiveButton; internal set => _positiveButton = value; }
+
+        private DialogResultButton _negativeButton;
+        public DialogResultButton NegativeButton { get => _negativeButton; internal set => _negativeButton = value; }
         
+        public DatePickerDialogViewModel(DatePickerDialog dialog)
+        {
+            _window = dialog;
+            ButtonClick = new RelayCommand(OnPressButton, CanPressButton);
+        }
+        
+        public bool CanPressButton(object args) => true;
+        public async void OnPressButton(object args)
+        {
+            var button = args as DialogResultButton;
+            if (button is null)
+                return; 
+
+            await Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                //_window.Result = result;
+                _window.Close();
+            });
+        }
+
+        public RelayCommand ButtonClick { get; private set; }
     }
 }
