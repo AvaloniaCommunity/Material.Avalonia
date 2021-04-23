86cd236ce9e75caa465510dc202e0806661bd6f9
AppleNeko2001 <appleneko2001@outlook.com>
2021-04-23 20:08:30 +0900

    Add datetime tostring converter

---
+++ Material.Dialog/Converters/DateStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Material.Dialog.Converters
+{
+    public class DateStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime)
+            {
+                var v = (DateTime)value;
+                var format = "ddd, MMM d";
+
+                return v.ToString(format);
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
