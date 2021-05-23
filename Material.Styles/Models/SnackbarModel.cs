2da6baf0ebb8e2a1c1a05884a67c94f97a8e6400
appleneko2001 <appleneko2001@outlook.com>
2021-05-23 10:59:43 +0900

    Initial snackbar system

---
+++ Material.Styles/Models/SnackbarModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Avalonia.Layout;
+
+namespace Material.Styles.Models
+{
+    public class SnackbarModel
+    {
+        public SnackbarModel(object content, Orientation orientation = Orientation.Horizontal)
+        {
+            _content = content;
+            _orientation = orientation;
+            //_button = button;
+        }
+        
+        private object _content;
+        public object Content => _content;
+
+        private Orientation _orientation;
+        public Orientation Orientation => _orientation;
+
+        // Still not sure I should use control instead of method call
+        // because I want more flexible button property on snackbar but
+        // that means we cant dismiss snackbar after pressed button.
+        private object _button;
+        public object Button => _button;
+
+        private TimeSpan _duration = TimeSpan.FromSeconds(5);
+        public TimeSpan Duration => _duration;
+    }
+}
