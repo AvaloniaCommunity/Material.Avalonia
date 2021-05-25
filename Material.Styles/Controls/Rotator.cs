9538d90609fd19a741afc9ecf989111ba5fed9d9
appleneko2001 <appleneko2001@outlook.com>
2021-05-25 19:08:21 +0900

    Use Avalonia render loop instead.

--- Material.Styles/Controls/Rotator.cs
+++ Material.Styles/Controls/Rotator.cs
@@ -1,78 +1,92 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
 using Avalonia;
+using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Media;
-using Avalonia.Threading;
+using Avalonia.Rendering;
+
+// ReSharper disable ConvertToLambdaExpression
 
 namespace Material.Styles.Controls
 {
     public class Rotator : ContentControl
     {
+        // Minimum speed
+        private double minimumSpeed = 0.0025;
+        private bool _running;
+
+        private double _speed = 0.4;
+
+        private double _rotateDegree = 0;
+
+        private RenderLoop _loop;
+        private RenderLoopClock _loopTask;
+        private TimeSpan _prev;
+
         public Rotator()
         {
-            _stopwatch = new Stopwatch();
-        }
+            // Idk how it does making any sense
+            // It works without any hooking / registering / attaching or etc.
+            // I just BLOW MY ****ING MIND
+            _loop = new RenderLoop();
 
-        public bool Rotate
-        {
-            get => _rotate;
-            set => SetAndRaise(RotateProperty, ref _rotate, value);
-        }
-        private bool _rotate;
-        
-        public static readonly DirectProperty<Rotator, bool> RotateProperty =
-            AvaloniaProperty.RegisterDirect<Rotator, bool>(nameof(Rotate),
-                delegate(Rotator rotator) { return rotator._rotate;},
-                delegate(Rotator rotator, bool b) { rotator._rotate = b; rotator.SetRotate(b); });
-        
-        public void SetRotate(bool v)
-        {
-            if (!v)
-            {
-                rotateDegree = 0;
-                _stopwatch.Stop();
-            }
-            else
-            {
-                _stopwatch.Start();
-                Task.Run(delegate
+            // Prepare render loop task for use.
+            _loopTask = new RenderLoopClock();
+            _loopTask.Subscribe(
+                delegate(TimeSpan renderTime)
                 {
-                    while (_rotate)
-                    {
-                        rotateDegree += speed * (_stopwatch.Elapsed - _prevTime).TotalMilliseconds;
-                        while (rotateDegree > 360)
-                            rotateDegree -= 360;
-                        
-                        _prevTime = _stopwatch.Elapsed;
-                        Dispatcher.UIThread.InvokeAsync(
-                            delegate
-                            {
-                                RenderTransform = new RotateTransform(rotateDegree);
-                            });
-                        
-                        Thread.Sleep(10);
-                    }
+                    var delta = renderTime - _prev;
+                    _rotateDegree += _speed * delta.TotalMilliseconds;
+                    _prev = renderTime;
+
+                    while (_rotateDegree > 360)
+                        _rotateDegree -= 360;
+
+                    RenderTransform = new RotateTransform(_rotateDegree);
                 });
-            }
         }
 
         public double Speed
         {
-            get => speed;
-            set => SetAndRaise(SpeedProperty, ref speed, value);
+            get => _speed;
+            set => SetAndRaise(SpeedProperty, ref _speed, value);
         }
-        
+
         public static readonly DirectProperty<Rotator, double> SpeedProperty =
-            AvaloniaProperty.RegisterDirect<Rotator, double>(nameof(Speed),
-                delegate(Rotator rotator) { return rotator.speed;},
-                delegate(Rotator rotator, double v) { rotator.speed = v; });
-        private double speed = 0.4;
-        
-        private double rotateDegree = 0;
-        private Stopwatch _stopwatch;
-        private TimeSpan _prevTime;
+            AvaloniaProperty.RegisterDirect(nameof(Speed),
+                delegate(Rotator rotator) { return rotator._speed; },
+                delegate(Rotator rotator, double v)
+                {
+                    rotator._speed = v;
+                    OnSpeedChanged(rotator, v);
+                });
+
+        // Loop dispatcher / simple loop controller
+        private static void OnSpeedChanged(Rotator rotator, double d)
+        {
+            // We should stop rotator if speed is lower than minimum speed
+            if (Math.Abs(d) < rotator.minimumSpeed)
+            {
+                // Stop render loop to avoid resources waste.
+                if (!rotator._running)
+                    return;
+
+                // Reset statements
+                rotator._running = false;
+                rotator._rotateDegree = 0;
+
+                // Detach loop task from RenderLoop
+                rotator._loop.Remove(rotator._loopTask);
+
+                return;
+            }
+
+            if (rotator._running)
+                return;
+
+            rotator._running = true;
+            // Attach loop task to RenderLoop
+            rotator._loop.Add(rotator._loopTask);
+        }
     }
 }
