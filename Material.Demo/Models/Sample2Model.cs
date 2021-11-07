f7469d96c75dae812b382fcff7d714500321a24b
SKProCH <skproch@yandex.ru>
2021-11-07 21:14:32 +0300

    Add DialogHost examples for demo

---
+++ Material.Demo/Models/Sample2Model.cs
@@ -0,0 +1,8 @@
+﻿namespace Material.Demo.Models {
+    public class Sample2Model {
+        public Sample2Model(int number) {
+            Number = number;
+        }
+        public int Number { get; set; }
+    }
+}
--- Material.Demo/Pages/DialogDemo.axaml.cs
+++ Material.Demo/Pages/DialogDemo.axaml.cs
@@ -1,6 +1,10 @@
-﻿using Avalonia;
+﻿using System;
+using System.Diagnostics;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Material.Demo.Models;
 using Material.Demo.ViewModels;
 using Material.Dialog;
 
@@ -20,6 +24,19 @@ namespace Material.Demo.Pages
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void OpenDialogWithView(object? sender, RoutedEventArgs e) {
+            DialogHost.DialogHost.Show(this.Resources["Sample2View"]!, "MainDialogHost");
+        }
+
+        private void OpenDialogWithModel(object? sender, RoutedEventArgs e) {
+            // View that associated with this model defined at DialogContentTemplate in DialogDemo.axaml
+            DialogHost.DialogHost.Show(new Sample2Model(new Random().Next(0, 100)), "MainDialogHost");
+        }
+
+        private void OpenMoreDialogHostExamples(object? sender, RoutedEventArgs e) {
+            Process.Start("https://github.com/AvaloniaUtils/DialogHost.Avalonia");
         }
     }
 }
