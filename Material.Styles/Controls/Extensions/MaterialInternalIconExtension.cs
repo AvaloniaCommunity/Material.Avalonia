cf1b757c41651f8a1ef0109baa10011610b4040d
appleneko2001 <appleneko2001@outlook.com>
2022-01-15 20:10:34 +0900

    Implement MaterialInternalIcon markup extension

---
+++ Material.Styles/Controls/Extensions/MaterialInternalIconExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Avalonia.Markup.Xaml;
+
+namespace Material.Styles.Controls.Extensions
+{
+    public class MaterialInternalIconExtension : MarkupExtension
+    {
+        public MaterialInternalIconExtension() { }
+        public MaterialInternalIconExtension(string kind) {
+            Kind = kind;
+        }
+
+        public MaterialInternalIconExtension(string kind, double? size) {
+            Kind = kind;
+            Size = size;
+        }
+        
+        [ConstructorArgument("kind")]
+        public string Kind { get; set; }
+
+        [ConstructorArgument("size")]
+        public double? Size { get; set; }
+        
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            var result = new MaterialInternalIcon
+            {
+                Kind = Kind
+            };
+
+            if (Size.HasValue)
+            {
+                result.Height = Size.Value;
+                result.Width = Size.Value;
+            }
+
+            return result;
+        }
+    }
+}
