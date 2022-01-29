50ce42151e991cfafd71db3e2b0456ee65d29c48
appleneko2001 <appleneko2001@outlook.com>
2022-01-29 17:29:53 +0900

    Simplify RangeToSweepConverter

--- Material.Styles/Converters/RangeToSweepConverter.cs
+++ Material.Styles/Converters/RangeToSweepConverter.cs
@@ -9,28 +9,21 @@ namespace Material.Styles.Converters
 {
     public class RangeToSweepConverter : IMultiValueConverter
     {
-        public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
         {
             double min = 0, max = 100, val = 0;
             
             if (values[0] is double value)
-            {
                 val = value;
-            }
 
             if (values.Count >= 2 && values[1] is double minimum)
-            {
                 min = minimum;
-            }
 
             if (values.Count >= 3 && values[2] is double maximum)
-            {
                 max = maximum;
-            }
 
-            double m = max - min;
-            var result = val / m;
-            return result * 360;
+            var m = max - min;
+            return val / m * 360;
         }
     }
 }
