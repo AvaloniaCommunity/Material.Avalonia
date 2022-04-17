1af911e4aa29879bae83988f573fe93e325a3bd6
appleneko2001 <appleneko2001@outlook.com>
2022-04-17 20:05:52 +0900

    Update ButtonAssist (Remove deprecated API, correct regions, nullable getter)

--- Material.Styles/Assists/ButtonAssist.cs
+++ Material.Styles/Assists/ButtonAssist.cs
@@ -1,49 +1,35 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
 using Avalonia.Media;
 
 namespace Material.Styles.Assists {
     public static class ButtonAssist {
-        private static readonly CornerRadius DefaultCornerRadius = new CornerRadius(4.0);
+        #region AttachedProperty : HoverColor
 
-        #region AttachedProperty : CornerRadiusProperty
-
-        /// <summary>
-        ///     Controls the corner radius of the surrounding box.
-        /// </summary>
-        public static readonly AvaloniaProperty<CornerRadius> CornerRadiusProperty = AvaloniaProperty.RegisterAttached<Button, CornerRadius>(
-            "CornerRadius", typeof(ButtonAssist), DefaultCornerRadius, true);
-
-        public static CornerRadius GetCornerRadius(AvaloniaObject element) {
-            return (CornerRadius) element.GetValue(CornerRadiusProperty);
-        }
-
-        public static void SetCornerRadius(AvaloniaObject element, CornerRadius value) {
-            element.SetValue(CornerRadiusProperty, value);
-        }
-        
-        
-        public static AvaloniaProperty<IBrush> HoverColorProperty = AvaloniaProperty.RegisterAttached<Button, IBrush>(
-            "HoverColor", typeof(ButtonAssist));
+        public static AvaloniaProperty<IBrush> HoverColorProperty = 
+            AvaloniaProperty.RegisterAttached<Button, IBrush>("HoverColor", typeof(ButtonAssist));
         
         public static void SetHoverColor(AvaloniaObject element, IBrush value) {
             element.SetValue(HoverColorProperty, value);
         }
 
-        public static IBrush GetHoverColor(AvaloniaObject element) {
-            return (IBrush) element.GetValue(HoverColorProperty);
+        public static IBrush? GetHoverColor(AvaloniaObject element) {
+            return element.GetValue(HoverColorProperty) as IBrush;
         }
+
+        #endregion
         
-        public static AvaloniaProperty<IBrush> ClickFeedbackColorProperty = AvaloniaProperty.RegisterAttached<Button, IBrush>(
-            "ClickFeedbackColor", typeof(ButtonAssist));
+        #region AttachedProperty : ClickFeedback
+
+        public static AvaloniaProperty<IBrush> ClickFeedbackColorProperty = 
+            AvaloniaProperty.RegisterAttached<Button, IBrush>("ClickFeedbackColor", typeof(ButtonAssist));
         
         public static void SetClickFeedbackColor(AvaloniaObject element, IBrush value) {
             element.SetValue(ClickFeedbackColorProperty, value);
         }
 
-        public static IBrush GetClickFeedbackColor(AvaloniaObject element) {
-            return (IBrush) element.GetValue(ClickFeedbackColorProperty);
+        public static IBrush? GetClickFeedbackColor(AvaloniaObject element) {
+            return element.GetValue(ClickFeedbackColorProperty) as IBrush;
         }
 
         #endregion
