88978ca7403f7020acb2b7e6deda10a070d93654
SKProCH <skproch@yandex.ru>
2022-05-09 19:29:22 +0300

    Bump DialogHost.Avalonia to 0.6.0-rc0

--- Material.Demo/Pages/DialogDemo.axaml.cs
+++ Material.Demo/Pages/DialogDemo.axaml.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using DialogHostAvalonia;
 using Material.Demo.Models;
 using Material.Demo.ViewModels;
 
@@ -23,12 +24,12 @@ namespace Material.Demo.Pages
         }
 
         private void OpenDialogWithView(object? sender, RoutedEventArgs e) {
-            DialogHost.DialogHost.Show(this.Resources["Sample2View"]!, "MainDialogHost");
+            DialogHost.Show(this.Resources["Sample2View"]!, "MainDialogHost");
         }
 
         private void OpenDialogWithModel(object? sender, RoutedEventArgs e) {
             // View that associated with this model defined at DialogContentTemplate in DialogDemo.axaml
-            DialogHost.DialogHost.Show(new Sample2Model(new Random().Next(0, 100)), "MainDialogHost");
+            DialogHost.Show(new Sample2Model(new Random().Next(0, 100)), "MainDialogHost");
         }
 
         private void OpenMoreDialogHostExamples(object? sender, RoutedEventArgs e) {
