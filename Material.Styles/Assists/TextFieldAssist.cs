d68f0e0889c6d8a6e20422a1db1b19992df491e5
appleneko2001 <appleneko2001@outlook.com>
2022-06-03 04:57:12 +0900

    Remove corner radius additional property in TextFieldAssist.cs

--- Material.Styles/Assists/TextFieldAssist.cs
+++ Material.Styles/Assists/TextFieldAssist.cs
@@ -1,23 +1,28 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Media;
 
 namespace Material.Styles.Assists {
     public static class TextFieldAssist
     {
-        public static AvaloniaProperty<string> HintsProperty = AvaloniaProperty.RegisterAttached<TextBox, string>(
+        public static readonly AvaloniaProperty<string?> HintsProperty = 
+            AvaloniaProperty.RegisterAttached<TextBox, string?>(
             "Hints", typeof(TextBox));
         
-        public static void SetHints(AvaloniaObject element, string value) => element.SetValue(HintsProperty, value);
+        public static void SetHints(AvaloniaObject element, string? value) => 
+            element.SetValue(HintsProperty, value);
 
-        public static string GetHints(AvaloniaObject element) => (string)element.GetValue(HintsProperty);
+        public static string? GetHints(AvaloniaObject element) => 
+            (string?)element.GetValue(HintsProperty);
 
-        public static AvaloniaProperty<string> LabelProperty = AvaloniaProperty.RegisterAttached<TextBox, string>(
+        public static readonly AvaloniaProperty<string?> LabelProperty = 
+            AvaloniaProperty.RegisterAttached<TextBox, string?>(
             "Label", typeof(TextBox));
 
-        public static void SetLabel(AvaloniaObject element, string value) => element.SetValue(LabelProperty, value);
+        public static void SetLabel(AvaloniaObject element, string? value) => 
+            element.SetValue(LabelProperty, value);
 
-        public static string GetLabel(AvaloniaObject element) => (string)element.GetValue(LabelProperty);
+        public static string? GetLabel(AvaloniaObject element) => 
+            (string?)element.GetValue(LabelProperty);
 
         // Use throw DataValidationException in property of view model instead of those things
         // Example can be found in dev-branch -> /Material.Demo/ViewModels/TextFieldsViewModel.cs
@@ -46,20 +51,5 @@ namespace Material.Styles.Assists {
         }
         
         */
-        private static readonly CornerRadius DefaultCornerRadius = new CornerRadius(0);
-        
-        /// <summary>
-        ///     Controls the corner radius of the surrounding box.
-        /// </summary>
-        public static readonly AvaloniaProperty<CornerRadius> CornerRadiusProperty = AvaloniaProperty.RegisterAttached<TextBox, CornerRadius>(
-            "CornerRadius", typeof(TextFieldAssist), DefaultCornerRadius, true);
-
-        public static CornerRadius GetCornerRadius(AvaloniaObject element) {
-            return (CornerRadius) element.GetValue(CornerRadiusProperty);
-        }
-
-        public static void SetCornerRadius(AvaloniaObject element, CornerRadius value) {
-            element.SetValue(CornerRadiusProperty, value);
-        }
     }
 }
