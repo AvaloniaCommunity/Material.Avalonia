f88a4e7976a76b3fabcf31817d2ee20261e49310
appleneko2001 <appleneko2001@outlook.com>
2022-06-03 04:58:09 +0900

    Formatting and improve ShadowAssist.cs

--- Material.Styles/Assists/ShadowAssist.cs
+++ Material.Styles/Assists/ShadowAssist.cs
@@ -9,21 +9,23 @@ using Avalonia.Styling;
 
 namespace Material.Styles.Assists {
     public static class ShadowProvider {
+        // ReSharper disable once MemberCanBePrivate.Global
+        // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Global
         public static Color MaterialShadowColor { get; set; } = Color.FromArgb(76, 0, 0, 0);
 
-        public static BoxShadows ToBoxShadows(this ShadowDepth shadowDepth, Color? overridedColor = null) {
+        public static BoxShadows ToBoxShadows(this ShadowDepth shadowDepth, Color? overrideColor = null) {
             return shadowDepth switch {
                 ShadowDepth.Depth0 => new BoxShadows(new BoxShadow()),
-                ShadowDepth.Depth1 => new BoxShadows(new BoxShadow {Blur = 5, OffsetX = 1, OffsetY = 1, Color = overridedColor ?? MaterialShadowColor}),
-                ShadowDepth.Depth2 => new BoxShadows(new BoxShadow {Blur = 8, OffsetX = 1.5, OffsetY = 1.5, Color = overridedColor ?? MaterialShadowColor}),
-                ShadowDepth.Depth3 => new BoxShadows(new BoxShadow {Blur = 14, OffsetX = 4.5, OffsetY = 4.5, Color = overridedColor ?? MaterialShadowColor}),
-                ShadowDepth.Depth4 => new BoxShadows(new BoxShadow {Blur = 25, OffsetX = 8, OffsetY = 8, Color = overridedColor ?? MaterialShadowColor}),
-                ShadowDepth.Depth5 => new BoxShadows(new BoxShadow {Blur = 35, OffsetX = 13, OffsetY = 13, Color = overridedColor ?? MaterialShadowColor}),
-                ShadowDepth.CenterDepth1 => new BoxShadows(new BoxShadow { Blur = 5, OffsetY = 1, Color = overridedColor ?? MaterialShadowColor }),
-                ShadowDepth.CenterDepth2 => new BoxShadows(new BoxShadow { Blur = 8, OffsetY = 1.5, Color = overridedColor ?? MaterialShadowColor }),
-                ShadowDepth.CenterDepth3 => new BoxShadows(new BoxShadow { Blur = 14, OffsetY = 4.5, Color = overridedColor ?? MaterialShadowColor }),
-                ShadowDepth.CenterDepth4 => new BoxShadows(new BoxShadow { Blur = 25, OffsetY = 8, Color = overridedColor ?? MaterialShadowColor }),
-                ShadowDepth.CenterDepth5 => new BoxShadows(new BoxShadow { Blur = 35, OffsetY = 13, Color = overridedColor ?? MaterialShadowColor }),
+                ShadowDepth.Depth1 => new BoxShadows(new BoxShadow {Blur = 5, OffsetX = 1, OffsetY = 1, Color = overrideColor ?? MaterialShadowColor}),
+                ShadowDepth.Depth2 => new BoxShadows(new BoxShadow {Blur = 8, OffsetX = 1.5, OffsetY = 1.5, Color = overrideColor ?? MaterialShadowColor}),
+                ShadowDepth.Depth3 => new BoxShadows(new BoxShadow {Blur = 14, OffsetX = 4.5, OffsetY = 4.5, Color = overrideColor ?? MaterialShadowColor}),
+                ShadowDepth.Depth4 => new BoxShadows(new BoxShadow {Blur = 25, OffsetX = 8, OffsetY = 8, Color = overrideColor ?? MaterialShadowColor}),
+                ShadowDepth.Depth5 => new BoxShadows(new BoxShadow {Blur = 35, OffsetX = 13, OffsetY = 13, Color = overrideColor ?? MaterialShadowColor}),
+                ShadowDepth.CenterDepth1 => new BoxShadows(new BoxShadow { Blur = 5, OffsetY = 1, Color = overrideColor ?? MaterialShadowColor }),
+                ShadowDepth.CenterDepth2 => new BoxShadows(new BoxShadow { Blur = 8, OffsetY = 1.5, Color = overrideColor ?? MaterialShadowColor }),
+                ShadowDepth.CenterDepth3 => new BoxShadows(new BoxShadow { Blur = 14, OffsetY = 4.5, Color = overrideColor ?? MaterialShadowColor }),
+                ShadowDepth.CenterDepth4 => new BoxShadows(new BoxShadow { Blur = 25, OffsetY = 8, Color = overrideColor ?? MaterialShadowColor }),
+                ShadowDepth.CenterDepth5 => new BoxShadows(new BoxShadow { Blur = 35, OffsetY = 13, Color = overrideColor ?? MaterialShadowColor }),
                 _                  => throw new ArgumentOutOfRangeException()
             };
         }
@@ -40,14 +42,16 @@ namespace Material.Styles.Assists {
         CenterDepth2,
         CenterDepth3,
         CenterDepth4,
-        CenterDepth5,
+        CenterDepth5
     }
 
     public static class ShadowAssist {
-        public static readonly AvaloniaProperty<ShadowDepth> ShadowDepthProperty = AvaloniaProperty.RegisterAttached<AvaloniaObject, ShadowDepth>(
+        public static readonly AvaloniaProperty<ShadowDepth> ShadowDepthProperty =
+            AvaloniaProperty.RegisterAttached<AvaloniaObject, ShadowDepth>(
             "ShadowDepth", typeof(ShadowAssist));
 
-        public static readonly AvaloniaProperty<bool> DarkenProperty = AvaloniaProperty.RegisterAttached<AvaloniaObject, bool>(
+        public static readonly AvaloniaProperty<bool> DarkenProperty = 
+            AvaloniaProperty.RegisterAttached<AvaloniaObject, bool>(
             "Darken", typeof(ShadowAssist));
 
         static ShadowAssist() {
@@ -56,42 +60,52 @@ namespace Material.Styles.Assists {
         }
 
         private static void ShadowDepthChangedCallback(AvaloniaPropertyChangedEventArgs args) {
-            if (args.Sender is Border border) border.BoxShadow = (args.NewValue as ShadowDepth? ?? ShadowDepth.Depth0).ToBoxShadows();
+            if (args.Sender is Border border)
+                border.BoxShadow = 
+                    (args.NewValue as ShadowDepth? ?? ShadowDepth.Depth0)
+                    .ToBoxShadows();
         }
 
-        public static void SetShadowDepth(AvaloniaObject element, ShadowDepth value) {
-            element.SetValue(ShadowDepthProperty, value);
-        }
+        public static void SetShadowDepth(AvaloniaObject element, ShadowDepth value) 
+            => element.SetValue<ShadowDepth>(ShadowDepthProperty, value);
 
-        public static ShadowDepth GetShadowDepth(AvaloniaObject element) {
-            return (ShadowDepth) element.GetValue(ShadowDepthProperty);
-        }
+        public static ShadowDepth GetShadowDepth(AvaloniaObject element) 
+            => element.GetValue<ShadowDepth>(ShadowDepthProperty);
 
-        private static void DarkenPropertyChangedCallback(AvaloniaPropertyChangedEventArgs obj) {
-            var border = obj.Sender as Border;
-            var boxShadow = border?.BoxShadow;
+        private static void DarkenPropertyChangedCallback(AvaloniaPropertyChangedEventArgs obj)
+        {
+            if (obj.Sender is not Border border)
+                return;
 
-            if (boxShadow == null) return;
+            var boxShadow = border.BoxShadow;
 
             var targetBoxShadows = (bool?) obj.NewValue == true
-                ? GetShadowDepth((AvaloniaObject) obj.Sender).ToBoxShadows(Color.FromArgb(255, 0, 0, 0))
-                : GetShadowDepth((AvaloniaObject) obj.Sender).ToBoxShadows();
+                ? GetShadowDepth(border).ToBoxShadows(Color.FromArgb(255, 0, 0, 0))
+                : GetShadowDepth(border).ToBoxShadows();
 
-            if (!border.Classes.Contains("notransitions"))
+            if (!border.Classes.Contains("no-transitions"))
             {
                 var animation = new Animation {Duration = TimeSpan.FromMilliseconds(350), FillMode = FillMode.Both};
                 animation.Children.Add(
                     new KeyFrame {
                         Cue = Cue.Parse("0%", CultureInfo.CurrentCulture),
                         Setters = {
-                            new Setter {Property = Border.BoxShadowProperty, Value = boxShadow}
+                            new Setter
+                            {
+                                Property = Border.BoxShadowProperty, 
+                                Value = boxShadow
+                            }
                         }
                     });
                 animation.Children.Add(
                     new KeyFrame {
                         Cue = Cue.Parse("100%", CultureInfo.CurrentCulture),
                         Setters = {
-                            new Setter {Property = Border.BoxShadowProperty, Value = targetBoxShadows}
+                            new Setter
+                            {
+                                Property = Border.BoxShadowProperty, 
+                                Value = targetBoxShadows
+                            }
                         }
                     });
                 animation.RunAsync(border, null, CancellationToken.None);
@@ -102,12 +116,10 @@ namespace Material.Styles.Assists {
             }
         }
 
-        public static void SetDarken(AvaloniaObject element, bool value) {
-            element.SetValue(DarkenProperty, value);
-        }
+        public static void SetDarken(AvaloniaObject element, bool value) 
+            => element.SetValue<bool>(DarkenProperty, value);
 
-        public static bool GetDarken(AvaloniaObject element) {
-            return (bool) element.GetValue(DarkenProperty);
-        }
+        public static bool GetDarken(AvaloniaObject element)
+            => element.GetValue<bool>(DarkenProperty);
     }
 }
