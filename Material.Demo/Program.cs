e3a76821852e20772a53a991445cd73e558ccf81
appleneko2001 <appleneko2001@outlook.com>
2022-09-11 11:27:45 +0900

    Disable Compositor temporarily because glitchy

--- Material.Demo/Program.cs
+++ Material.Demo/Program.cs
@@ -33,8 +33,17 @@ namespace Material.Demo
                 })
                 .With(new Win32PlatformOptions
                 {
-                    // Avalonia 11.0.0-preview1 issue: CornerRadius not clipping
-                    UseCompositor = true
+                    // Avalonia 11.0.0-preview1 issue: CornerRadius not clipping,
+                    // Avalonia 11.0.0-preview1 issue: sometimes might crash by collection enumerate fail
+                    UseCompositor = false
+                })
+                .With(new X11PlatformOptions
+                {
+                    UseCompositor = false
+                })
+                .With(new AvaloniaNativePlatformOptions
+                {
+                    UseCompositor = false
                 })
                 .LogToTrace();
         }
