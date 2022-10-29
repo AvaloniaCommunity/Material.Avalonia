d21f8aa8f5464245b775dc5fb63342d9fb1bd184
appleneko2001 <appleneko2001@outlook.com>
2022-10-29 18:20:51 +0900

    Null-safe DatePickerTextConverter.cs

--- Material.Styles/Converters/DatePickerTextConverter.cs
+++ Material.Styles/Converters/DatePickerTextConverter.cs
@@ -9,19 +9,19 @@ namespace Material.Styles.Converters
 {
     public class DatePickerTextConverter : IMultiValueConverter
     {
-        public static DatePickerTextConverter Instance { get; } = new DatePickerTextConverter();
+        public static DatePickerTextConverter Instance { get; } = new();
 
-        public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
         {
-            // ReSharper disable once ConditionIsAlwaysTrueOrFalse
-            // ReSharper disable once HeuristicUnreachableCode
             try
             {
                 return values[0] is UnsetValueType || values[0] == null
                     ? "Not selected"
-                    : ((DateTimeOffset) values[0]).ToString((string) values[1]);
+                    : values[0] is DateTimeOffset offset
+                        ? offset.ToString(values[1] as string)
+                        : BindingOperations.DoNothing;
             }
-            catch (Exception)
+            catch
             {
                 return BindingOperations.DoNothing;
             }
