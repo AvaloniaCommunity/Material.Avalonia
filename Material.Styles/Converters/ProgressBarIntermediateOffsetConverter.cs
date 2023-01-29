a26571424a9bf540ffb4bf778b8552e3866ac20a
SKProCH <skproch@yandex.ru>
2023-01-29 21:09:36 +0300

    Rework of default ProgressBar theme

---
+++ Material.Styles/Converters/ProgressBarIntermediateOffsetConverter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Material.Styles.Converters;
+
+public sealed class ProgressBarIntermediateOffsetConverter : IValueConverter {
+    /// <inheritdoc />
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        return (double)value! * System.Convert.ToDouble(parameter, NumberFormatInfo.InvariantInfo);
+    }
+    /// <inheritdoc />
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        throw new NotSupportedException();
+    }
+}
