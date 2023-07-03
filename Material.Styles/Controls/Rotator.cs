df4146ac3f0651a5d90c071d9496e84f98395edc
SKProCH <skproch@yandex.ru>
2023-07-04 01:03:06 +0300

    Optimise Rotator usage with new RequestAnimationFrame API

--- Material.Styles/Controls/Rotator.cs
+++ Material.Styles/Controls/Rotator.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
@@ -9,11 +8,6 @@ using Avalonia.Media;
 namespace Material.Styles.Controls;
 
 public class Rotator : Panel {
-    // Minimum speed
-    // Rotator will stop if speed less than this constant value.
-    // It is able to change in code-behind if this value is not satisfied.
-    private static double _minimumSpeed = 0.0025;
-
     public static readonly DirectProperty<Rotator, double> SpeedProperty =
         AvaloniaProperty.RegisterDirect(nameof(Speed),
             rotator => rotator._speed,
@@ -22,30 +16,21 @@ public class Rotator : Panel {
                     return;
 
                 rotator._speed = v;
-                rotator.OnSpeedChanged(rotator, v);
+                rotator.RequestAnimationIfNeeded();
             });
-    private RotateTransform _renderTransform;
+
+    private readonly RotateTransform _renderTransform;
+    private bool _isAnimationActionRunning;
+    private TimeSpan _lastRenderTime = TimeSpan.Zero;
     private double _rotateDegree;
-    private bool _running;
     private double _speed = 0.4;
 
-    private Stopwatch _stopwatch = new();
+    static Rotator() {
+        IsEffectivelyEnabledProperty.Changed.AddClassHandler<Rotator>((o, _) => o.RequestAnimationIfNeeded());
+    }
 
     public Rotator() {
         RenderTransform = _renderTransform = new RotateTransform();
-    }
-
-    // ReSharper disable once MemberCanBePrivate.Global
-    public static double MinimumSpeed {
-        get => _minimumSpeed;
-        set {
-            if (value < 0) {
-                throw new ArgumentOutOfRangeException(nameof(value),
-                    "MinimumSpeed should not less than zero. You can set it as zero, if you wish your rotator keep running.");
-            }
-
-            _minimumSpeed = value;
-        }
     }
 
     public double Speed {
@@ -53,46 +38,31 @@ public class Rotator : Panel {
         set => SetAndRaise(SpeedProperty, ref _speed, value);
     }
 
-    private void OnSpeedChanged(Rotator rotator, double newSpeed) {
-        _rotateDegree += newSpeed * _stopwatch.Elapsed.TotalMilliseconds;
-        _stopwatch.Restart();
-        rotator._renderTransform.Angle = _rotateDegree % 360;
+    private void RequestAnimationIfNeeded() {
+        if (_isAnimationActionRunning) return;
+        _isAnimationActionRunning = true;
+        TopLevel.GetTopLevel(this)?.RequestAnimationFrame(OnAnimationFrame);
     }
 
-    // Old behavior
-    // TODO: in next avalonia release use RequestAnimationFrame https://github.com/AvaloniaUI/Avalonia/issues/11627
-    // private void OnLoopUpdate(TimeSpan renderTime) {
-    //     if (IsEffectivelyVisible == false || IsEffectivelyEnabled == false)
-    //         return;
-    //
-    //     var delta = renderTime - _prev;
-    //     _rotateDegree += _speed * delta.TotalMilliseconds;
-    //     _prev = renderTime;
-    //
-    //     _rotateDegree %= 360;
-    //
-    //     _renderTransform.Angle = _rotateDegree;
-    // }
+    private void OnAnimationFrame(TimeSpan renderTime) {
+        if (IsEffectivelyVisible == false || IsEffectivelyEnabled == false || _speed == 0) {
+            _isAnimationActionRunning = false;
+            return;
+        }
 
-    // private static void OnSpeedChanged(Rotator rotator, double newSpeed) {
-    //     // We should stop rotator if speed is lower than minimum speed
-    //     if (Math.Abs(newSpeed) < _minimumSpeed) {
-    //         // Stop render loop to avoid leak of performances.
-    //         if (!rotator._running)
-    //             return;
-    //
-    //         // Reset statements
-    //         rotator._running = false;
-    //         rotator._rotateDegree = 0;
-    //
-    //         // Detach loop task from RenderLoop
-    //         return;
-    //     }
-    //
-    //     if (rotator._running)
-    //         return;
-    //
-    //     // Attach loop task to RenderLoop if not running.
-    //     rotator._running = true;
-    // }
+        var delta = renderTime - _lastRenderTime;
+        _rotateDegree += _speed * delta.TotalMilliseconds;
+
+        _lastRenderTime = renderTime;
+        _rotateDegree %= 360;
+        _renderTransform.Angle = _rotateDegree;
+
+        TopLevel.GetTopLevel(this)?.RequestAnimationFrame(OnAnimationFrame);
+    }
+
+    /// <inheritdoc />
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e) {
+        base.OnAttachedToVisualTree(e);
+        RequestAnimationIfNeeded();
+    }
 }
