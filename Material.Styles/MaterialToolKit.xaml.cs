2b2ef3812b78e7290b4d84e6344ccff560108f62
SKProCH <skproch@yandex.ru>
2023-08-09 21:43:03 +0300

    Add auto-including Material.DataGrid styles if present

--- Material.Styles/MaterialToolKit.xaml.cs
+++ Material.Styles/MaterialToolKit.xaml.cs
@@ -1,16 +1,26 @@
+using System;
 using Avalonia;
 using Avalonia.Animation;
 using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
 using Material.Styles.Additional;
 
-namespace Material.Styles {
-    public class MaterialToolKit : Avalonia.Styling.Styles {
-        static MaterialToolKit() {
+namespace Material.Styles
+{
+    public class MaterialToolKit : Avalonia.Styling.Styles
+    {
+        static MaterialToolKit()
+        {
             Animation.RegisterCustomAnimator<RelativePoint, RelativePointAnimator>();
         }
 
-        public MaterialToolKit() {
+        public MaterialToolKit()
+        {
             AvaloniaXamlLoader.Load(this);
+            if (AppContext.TryGetSwitch("MaterialThemeIncludeDataGrid", out var includeDataGrid) && includeDataGrid)
+            {
+                Add(new StyleInclude((Uri?)null) { Source = new Uri("avares://Material.DataGrid/DataGrid.xaml") });
+            }
         }
     }
 }
