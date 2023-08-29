8ec03eb6078a8dfe842478142340abe770948af3
appleneko2001 <appleneko2001@outlook.com>
2023-08-30 00:08:00 +0900

    [DialogViewModelBase] Suppress nullability warning by applying fix

--- Material.Dialog/ViewModels/ViewModelBase.cs
+++ Material.Dialog/ViewModels/ViewModelBase.cs
@@ -5,9 +5,9 @@ namespace Material.Dialog.ViewModels
 {
     public class DialogViewModelBase : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
