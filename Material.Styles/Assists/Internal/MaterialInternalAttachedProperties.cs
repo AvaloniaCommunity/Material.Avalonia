4c98c2c46be2a4477157e5b30351e9ea8bae7921
SKProCH <skproch@yandex.ru>
2023-09-22 02:15:47 +0300

    Add internal attached properties (for textbox)

---
+++ Material.Styles/Assists/Internal/MaterialInternalAttachedProperties.cs
@@ -0,0 +1,23 @@
+using Avalonia;
+using Avalonia.Controls;
+
+namespace Material.Styles.Assists.Internal;
+
+/// <summary>
+/// Contains some internal properties for Material.Avalonia
+/// </summary>
+/// <remarks>
+/// You probably SHOULDN'T USE THIS
+/// </remarks>
+public static class MaterialInternalAttachedProperties {
+    public static readonly AttachedProperty<bool> UseClippingForBorderProperty
+        = AvaloniaProperty.RegisterAttached<TextBox, bool>("UseClippingForBorder", typeof(MaterialInternalAttachedProperties));
+
+    public static bool GetUseClippingForBorder(TextBox element) {
+        return element.GetValue(UseClippingForBorderProperty);
+    }
+
+    public static void SetUseClippingForBorder(TextBox element, bool value) {
+        element.SetValue(UseClippingForBorderProperty, value);
+    }
+}
