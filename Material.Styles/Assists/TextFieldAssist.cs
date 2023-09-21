5768fcc8b5d249a44500f79e21c7b2a863bde12c
SKProCH <skproch@yandex.ru>
2023-09-22 02:14:35 +0300

    Adds new field assist properties

--- Material.Styles/Assists/TextFieldAssist.cs
+++ Material.Styles/Assists/TextFieldAssist.cs
@@ -1,56 +1,55 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Material.Styles.Assists.Mixins;
 
-namespace Material.Styles.Assists
-{
-    public static class TextFieldAssist
-    {
-        public static readonly AvaloniaProperty<string?> HintsProperty =
-            AvaloniaProperty.RegisterAttached<TemplatedControl, string?>(
-                "Hints", typeof(TemplatedControl));
+namespace Material.Styles.Assists;
 
-        public static void SetHints(AvaloniaObject element, string? value) =>
-            element.SetValue(HintsProperty, value);
+public static class TextFieldAssist {
+    public static readonly AvaloniaProperty<string?> HintsProperty =
+        AvaloniaProperty.RegisterAttached<TemplatedControl, string?>("Hints", typeof(TextFieldAssist));
 
-        public static string? GetHints(AvaloniaObject element) =>
-            element.GetValue<string?>(HintsProperty);
+    public static readonly AvaloniaProperty<string?> LabelProperty =
+        AvaloniaProperty.RegisterAttached<TemplatedControl, string?>("Label", typeof(TemplatedControl));
 
-        public static readonly AvaloniaProperty<string?> LabelProperty =
-            AvaloniaProperty.RegisterAttached<TemplatedControl, string?>(
-                "Label", typeof(TemplatedControl));
+    public static readonly AttachedProperty<string?> WatermarkProperty =
+        AvaloniaProperty.RegisterAttached<TemplatedControl, string?>("Watermark", typeof(TextFieldAssist));
 
-        public static void SetLabel(AvaloniaObject element, string? value) =>
-            element.SetValue(LabelProperty, value);
+    public static readonly AttachedProperty<bool> UseFloatingLabelProperty =
+        AvaloniaProperty.RegisterAttached<TemplatedControl, bool>("UseFloatingLabel", typeof(TextFieldAssist));
+    static TextFieldAssist() {
+        WatermarkAsLabelMixin.Attach<TextBox>();
+    }
 
-        public static string? GetLabel(AvaloniaObject element) =>
-            element.GetValue<string?>(LabelProperty);
+    public static void SetHints(TemplatedControl element, string? value) {
+        element.SetValue(HintsProperty, value);
+    }
 
-        // Use throw DataValidationException in property of view model instead of those things
-        // Example can be found in dev-branch -> /Material.Demo/ViewModels/TextFieldsViewModel.cs
+    public static string? GetHints(TemplatedControl element) {
+        return element.GetValue<string?>(HintsProperty);
+    }
 
-        /*
-        
-        public static AvaloniaProperty<bool> HasErrorProperty = AvaloniaProperty.RegisterAttached<TextBox, bool>(
-            "HasError", typeof(TextBox));
+    public static void SetLabel(TemplatedControl element, string? value) {
+        element.SetValue(LabelProperty, value);
+    }
 
-        public static void SetHasError(AvaloniaObject element, bool value) => element.SetValue(HasErrorProperty, value);
+    public static string? GetLabel(TemplatedControl element) {
+        return element.GetValue<string?>(LabelProperty);
+    }
 
-        public static bool GetHasError(AvaloniaObject element) => (bool)element.GetValue(HasErrorProperty);
+    public static string? GetWatermark(TemplatedControl element) {
+        return element.GetValue(WatermarkProperty);
+    }
 
+    public static void SetWatermark(TemplatedControl element, string? value) {
+        element.SetValue(WatermarkProperty, value);
+    }
 
-        public static AvaloniaProperty<SolidColorBrush> ErrorColorProperty = 
-            AvaloniaProperty.RegisterAttached<TextBox, SolidColorBrush>("ErrorColor", typeof(TextFieldAssist), SolidColorBrush.Parse("#f44336"));
+    public static bool GetUseFloatingLabel(TemplatedControl element) {
+        return element.GetValue(UseFloatingLabelProperty);
+    }
 
-        public static void SetErrorColor(AvaloniaObject element, SolidColorBrush value)
-        {
-            element.SetValue(ErrorColorProperty, value);
-        }
-
-        public static SolidColorBrush GetErrorColor(AvaloniaObject element)
-        {
-            return (SolidColorBrush)element.GetValue(ErrorColorProperty);
-        }
-        
-        */
+    public static void SetUseFloatingLabel(TemplatedControl element, bool value) {
+        element.SetValue(UseFloatingLabelProperty, value);
     }
 }
