b7d506fb6a0ca7f4f7bae775e40f920490daf080
SKProCH <skproch@yandex.ru>
2024-01-13 02:39:06 +0300

    Fix MaterialBorderBoxShadowMixin transitions apply logic

--- Material.Styles/Assists/Mixins/MaterialBorderBoxShadowMixin.cs
+++ Material.Styles/Assists/Mixins/MaterialBorderBoxShadowMixin.cs
@@ -9,7 +9,7 @@ namespace Material.Styles.Assists.Mixins;
 /// Adds BoxShadows transitions for any control, which has enabled animations.
 /// </summary>
 public static class MaterialBorderBoxShadowMixin {
-    public static BoxShadowsTransition TargetTransition { get; set; } = new() { Property = Border.BoxShadowProperty, Duration = TimeSpan.FromMilliseconds(350) };
+    public static BoxShadowsTransition TargetTransition { get; set; } = new() { Property = Border.BoxShadowProperty, Duration = TimeSpan.FromMilliseconds(3500) };
 
     /// <summary>
     /// Initializes BoxShadows transitions for specified control.
@@ -26,17 +26,20 @@ public static class MaterialBorderBoxShadowMixin {
         if (transitions is null) return;
 
         var disableTransitions = args.GetNewValue<bool>();
-        if (disableTransitions)
-            transitions.Remove(TargetTransition);
-        else
-            transitions.Add(TargetTransition);
+        ToggleTransitions(transitions, disableTransitions);
     }
 
     private static void OnTransitionCollectionChanged<TControl>(TControl control, AvaloniaPropertyChangedEventArgs args)
         where TControl : Control {
         var disableTransitions = (bool)control.GetValue(TransitionAssist.DisableTransitionsProperty)!;
-        if (disableTransitions) return;
+        if (args.NewValue is Transitions transitions)
+            ToggleTransitions(transitions, disableTransitions);
+    }
 
-        if (args.NewValue is Transitions transitions && !transitions.Contains(TargetTransition)) transitions.Add(TargetTransition);
+    private static void ToggleTransitions(Transitions transitions, bool disableTransitions) {
+        if (disableTransitions)
+            transitions.Remove(TargetTransition);
+        else if (!transitions.Contains(TargetTransition))
+            transitions.Add(TargetTransition);
     }
 }
