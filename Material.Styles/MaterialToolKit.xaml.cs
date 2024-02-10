f00728c1404ca3fbe64a59fba32e3d3fc8cfff40
SKProCH <skproch@yandex.ru>
2024-02-11 02:08:21 +0300

    Enable NativeAot support for all projects, add net8.0 target, rewrite DataGrid styles autoincluding in MaterialToolKit #232

--- Material.Styles/MaterialToolKit.xaml.cs
+++ Material.Styles/MaterialToolKit.xaml.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using Avalonia;
 using Avalonia.Animation;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using Avalonia.Markup.Xaml.Styling;
 using Material.Styles.Additional;
 using Material.Styles.Assists.Mixins;
 
@@ -14,11 +15,29 @@ namespace Material.Styles {
             MaterialBorderBoxShadowMixin.Attach<Border>();
         }
 
+
         public MaterialToolKit() {
             AvaloniaXamlLoader.Load(this);
-            if (AppContext.TryGetSwitch("MaterialThemeIncludeDataGrid", out var includeDataGrid) && includeDataGrid) {
-                Add(new StyleInclude((Uri?)null) { Source = new Uri("avares://Material.Avalonia.DataGrid/DataGrid.xaml") });
-            }
+            IncludeDataGridStyles();
+        }
+
+
+#pragma warning disable IL2072
+
+#if NET5_0_OR_GREATER
+        [DynamicDependency(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor, "MaterialDataGridStyles",
+            "Material.Avalonia.DataGrid")]
+        [UnconditionalSuppressMessage("Trimming",
+            "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code",
+            Justification = "Referenced by DynamicDependency")]
+#endif
+        private void IncludeDataGridStyles() {
+            if (!AppContext.TryGetSwitch("MaterialThemeIncludeDataGrid", out var includeDataGrid) ||
+                !includeDataGrid) return;
+            var dataGridStylesType = Assembly.Load("Material.Avalonia.DataGrid")
+                .GetType("Material.Avalonia.DataGrid.MaterialDataGridStyles")!;
+            var instance = Activator.CreateInstance(dataGridStylesType)!;
+            Add((Avalonia.Styling.Styles)instance);
         }
     }
 }
