e9922490004596969503b7e7620fcc4e748d5b8a
SKProCH <skproch@yandex.ru>
2024-02-11 01:32:06 +0300

    Make demo use compiled bindings

--- Material.Demo/Pages/ProgressIndicatorDemo.axaml.cs
+++ Material.Demo/Pages/ProgressIndicatorDemo.axaml.cs
@@ -2,70 +2,14 @@
 using System.Timers;
 using Avalonia;
 using Avalonia.Controls;
+using Material.Demo.ViewModels;
 
 namespace Material.Demo.Pages {
     public partial class ProgressIndicatorDemo : UserControl {
-        private readonly Context context;
-        private readonly Timer timer;
-        private int caseProgress;
-
         public ProgressIndicatorDemo() {
             InitializeComponent();
 
-            timer = new Timer(1000);
-            timer.Elapsed += Timer_Elapsed;
-
-            DataContext = context = new Context();
-
-            AttachedToVisualTree += ProgressIndicatorDemo_AttachedToVisualTree;
-        }
-
-        private void ProgressIndicatorDemo_AttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e) {
-            timer.Start();
-        }
-
-        private void Timer_Elapsed(object? sender, ElapsedEventArgs e) {
-            context.Progress = SwitchProgress();
-        }
-
-        private double SwitchProgress() {
-            switch (caseProgress) {
-                case 0:
-                    caseProgress++;
-                    return 30;
-                case 1:
-                    caseProgress++;
-                    return 45;
-                case 2:
-                    caseProgress++;
-                    return 50;
-                case 3:
-                    caseProgress++;
-                    return 80;
-                case 4:
-                    caseProgress++;
-                    return 100;
-                case 5:
-                    caseProgress = 0;
-                    return 0;
-                default:
-                    caseProgress = 0;
-                    return 0;
-            }
-        }
-
-        public class Context : INotifyPropertyChanged {
-            private double m_Progress;
-
-            public double Progress {
-                get => m_Progress;
-                set {
-                    m_Progress = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Progress)));
-                }
-            }
-
-            public event PropertyChangedEventHandler? PropertyChanged;
+            DataContext = new ProgressIndicatorDemoViewModel();
         }
     }
 }
