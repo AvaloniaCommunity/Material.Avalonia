ea3ce1282dc22431e965d104c7049a67abae8e00
SKProCH <skproch@yandex.ru>
2024-04-07 16:51:32 +0300

    Updates the wasm demo to nightly avalonia

--- Material.Avalonia.Demo.Browser/AppBundle/main.js
+++ Material.Avalonia.Demo.Browser/AppBundle/main.js
@@ -1,4 +1,4 @@
-﻿import { dotnet } from './dotnet.js'
+﻿import { dotnet } from './_framework/dotnet.js'
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
