b45ad7546e185a158293d5c062e488558b784a0b
Corvin <corvinszimion@googlemail.com>
2025-04-04 19:54:34 +0200

    added FieldsLineUpDemo page to the demo app

---
+++ Material.Avalonia.Demo/Pages/FieldsLineUpDemo.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml;
+using Material.Avalonia.Demo.ViewModels;
+
+namespace Material.Avalonia.Demo.Pages;
+
+public partial class FieldsLineUpDemo : UserControl {
+    public FieldsLineUpDemo() {
+        InitializeComponent();
+    }
+}
