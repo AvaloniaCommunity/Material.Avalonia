2351052008a1724268fc1fb00c8f10f874333697
appleneko2001 <appleneko2001@outlook.com>
2025-05-01 22:37:55 +0900

    Add button assist: memorise clicked (IsClicked)

--- Material.Styles/Assists/ButtonAssist.cs
+++ Material.Styles/Assists/ButtonAssist.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Media;
+using Material.Styles.Internal;
 
 namespace Material.Styles.Assists
 {
@@ -29,6 +31,49 @@ namespace Material.Styles.Assists
 
         public static IBrush? GetClickFeedbackColor(AvaloniaObject element) =>
             element.GetValue<IBrush?>(ClickFeedbackColorProperty);
+
+        #endregion
+        
+        // Used for memorise which hyperlink button clicked (or just say, visited)
+        #region AttachedProperty : IsClicked
+
+        public static readonly AvaloniaProperty<bool?> IsClickedProperty =
+            AvaloniaProperty.RegisterAttached<Button, bool?>("IsClicked", typeof(ButtonAssist));
+
+        public static void SetIsClicked(AvaloniaObject element, bool? value) =>
+            element.SetValue(IsClickedProperty, value);
+
+        public static bool? GetIsClicked(AvaloniaObject element) =>
+            element.GetValue<bool?>(IsClickedProperty);
+
+        
+        private static void OnButtonClickedPrivate((object, RoutedEventArgs) args) {
+            if(args.Item1 is not Button button)
+                return;
+
+            var value = button.GetValue<bool?>(IsClickedProperty);
+            
+            // null means not required for handling
+            if(!value.HasValue)
+                return;
+
+            // if IsClickedProperty is false, put it to true
+            if (!value.Value)
+                button.SetValue(IsClickedProperty, true);
+            
+            if (button is not HyperlinkButton hyperlink)
+                return;
+
+            hyperlink.IsVisited = true;
+        }
+
+        #endregion
+
+        #region Initiator (used for observe control changes)
+
+        static ButtonAssist() {
+            Button.ClickEvent.Raised.Subscribe(OnButtonClickedPrivate);
+        }
 
         #endregion
     }
