a722eda49a3e5abf3dbefc3369c4137174c605cd
appleneko2001 <appleneko2001@outlook.com>
2025-05-01 22:39:18 +0900

    Add button demo: HyperlinkButton

--- Material.Avalonia.Demo/Pages/ButtonsDemo.axaml.cs
+++ Material.Avalonia.Demo/Pages/ButtonsDemo.axaml.cs
@@ -1,9 +1,18 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
 
 namespace Material.Avalonia.Demo.Pages;
 
 public partial class ButtonsDemo : UserControl {
     public ButtonsDemo() {
         InitializeComponent();
+    }
+
+    public void OnHyperlink1Clicked() {
+        Console.WriteLine("Hyperlink #1 CLICKED!");
+    }
+    
+    public void OnHyperlink2Clicked() {
+        Console.WriteLine("Another Hyperlink is CLICKED!");
     }
 }
