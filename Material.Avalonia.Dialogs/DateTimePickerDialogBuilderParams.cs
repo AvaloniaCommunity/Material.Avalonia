c9db945c8edaecad2f32b593b28ff053357fb616
SKProCH <29896317+SKProCH@users.noreply.github.com>
2025-08-01 01:22:31 +0300

    Add obsoletion messages for DatePickerDialog and TimePickerDialog (#471)
    https://github.com/AvaloniaCommunity/Material.Avalonia/issues/470

--- Material.Avalonia.Dialogs/DateTimePickerDialogBuilderParams.cs
+++ Material.Avalonia.Dialogs/DateTimePickerDialogBuilderParams.cs
@@ -3,6 +3,8 @@ using Material.Dialog.Bases;
 
 namespace Material.Dialog
 {
+    // TODO Fix DatePicker TimePicker dialogs https://github.com/AvaloniaCommunity/Material.Avalonia/issues/470
+    [Obsolete("Currently unsupported - https://github.com/AvaloniaCommunity/Material.Avalonia/issues/470")]
     public class DatePickerDialogBuilderParams : TwoFeedbackDialogBuilderParamsBase
     {
         /// <summary>
@@ -11,6 +13,7 @@ namespace Material.Dialog
         public DateTime ImplicitValue = DateTime.Now;
     }
 
+    [Obsolete("Currently unsupported - https://github.com/AvaloniaCommunity/Material.Avalonia/issues/470")]
     public class TimePickerDialogBuilderParams : TwoFeedbackDialogBuilderParamsBase
     {
         /// <summary>
