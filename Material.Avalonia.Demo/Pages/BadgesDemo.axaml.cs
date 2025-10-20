4c3e143aa539107d34fb4fde5f23a1d51799e3fb
SKProCH <skproch@yandex.ru>
2025-10-20 23:53:18 +0300

    Add badge increment sample in demo

--- Material.Avalonia.Demo/Pages/BadgesDemo.axaml.cs
+++ Material.Avalonia.Demo/Pages/BadgesDemo.axaml.cs
@@ -1,14 +1,22 @@
 using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Material.Styles.Controls;
 
 namespace Material.Avalonia.Demo.Pages;
 
 public partial class BadgesDemo : UserControl {
+    private int _inboxCount;
+
     public BadgesDemo() {
         InitializeComponent();
         badgePlacementSelector.ItemsSource = Enum.GetValues<BadgePlacement>();
         badgePlacementSelector.SelectedItem = BadgePlacement.TopRight;
+    }
+
+    private void OnIncrementBadgeClick(object? sender, RoutedEventArgs e) {
+        _inboxCount++;
+        interactiveBadged.BadgeContent = _inboxCount.ToString();
     }
 }
 
