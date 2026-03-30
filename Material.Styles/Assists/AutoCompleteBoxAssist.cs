db727733e205a3f08804a58e77bf76fdc604cd3d
Sebastian Klatte <sebastian.klatte@qonlab.com>
2026-03-30 11:17:57 +0200

    - Improve AutoCompleteBox styles. - Add AutoCompleteBox support for InnerLeftContent and InnerRightContent. - Add AutoCompleteBox support for Label and UseFloatingWatermark.

---
+++ Material.Styles/Assists/AutoCompleteBoxAssist.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace Material.Styles.Assists {
+    public static class AutoCompleteBoxAssist {
+        public static readonly AttachedProperty<bool> UseFloatingWatermarkProperty =
+            AvaloniaProperty.RegisterAttached<AutoCompleteBox, bool>(
+                "UseFloatingWatermark", typeof(AutoCompleteBox), false);
+
+        public static void SetUseFloatingWatermark(AvaloniaObject element, bool value) =>
+            element.SetValue(UseFloatingWatermarkProperty, value);
+
+        public static bool GetUseFloatingWatermark(AvaloniaObject element) =>
+            element.GetValue(UseFloatingWatermarkProperty);
+    }
+}
